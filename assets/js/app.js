73835f7360bbb7744d24a8d0c2b606a1cb354ade
joongau <gaultier.jonathan@gmail.com>
2020-05-12 13:20:13 +0200

    add test carousel

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,9 +5,10 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.scss in this case)
+// any CSS you import will output into a single css file (_bootswatch.scss in this case)
 import '../css/app.scss';
 import "bootswatch/dist/darkly/bootstrap.min.css";
+import "_carousel"
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
